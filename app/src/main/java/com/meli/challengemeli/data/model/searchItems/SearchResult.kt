62b6a2763d324f9cd308fcc53b0d66a7fb49f9f8
Pablo <pablo.inoriza@despegar.com>
2021-05-30 20:23:30 -0300

    Agrego las clases de retrofit

--- app/src/main/java/com/meli/challengemeli/data/model/searchItems/SearchResult.kt
+++ app/src/main/java/com/meli/challengemeli/data/model/searchItems/SearchResult.kt
@@ -1,3 +1,3 @@
 package com.meli.challengemeli.data.model.searchItems
 
-data class SearchItems(val results: List<Result>)
+data class SearchResult(val results: List<Result>)
---
+++ app/src/main/java/com/meli/challengemeli/networking/WebService.kt
@@ -0,0 +1,20 @@
+package com.meli.challengemeli.networking
+
+import com.meli.challengemeli.data.model.searchItems.SearchResult
+import com.meli.challengemeli.util.Site
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface WebService {
+
+    @GET("$SITES_URL_SEGMENT/$SITE_ID/$SEARCH_URL_SEGMENT")
+    fun getSearchResults(@Path(SITE_ID) siteId: Site, @Query(Q_PARAM) searchText: String): SearchResult
+
+    companion object {
+        private const val SITE_ID = "SITE_ID"
+        private const val Q_PARAM = "q"
+        private const val SITES_URL_SEGMENT = "sites"
+        private const val SEARCH_URL_SEGMENT = "search"
+    }
+}
