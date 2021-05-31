06ec6e612420567c3c270e8c61cb9fcd73820166
Pablo Inoriza <pablo-ino@hotmail.com>
2021-05-31 13:33:12 -0300

    Unifico los data class del model en un solo archivo (#4)

--- app/src/main/java/com/meli/challengemeli/data/model/Result.kt
+++
@@ -1,5 +0,0 @@
-package com.meli.challengemeli.data.model
-
-import com.meli.challengemeli.data.model.Attribute
-
-data class Result(val title: String, val price: Int, val thumbnail: String, val attributes: List<Attribute>)
--- app/src/main/java/com/meli/challengemeli/data/model/SearchResults.kt
+++ app/src/main/java/com/meli/challengemeli/data/model/SearchResults.kt
@@ -1,3 +1,7 @@
 package com.meli.challengemeli.data.model
 
 data class SearchResults(val results: List<Result>)
+
+data class Result(val title: String, val price: Int, val thumbnail: String, val attributes: List<Attribute>)
+
+data class Attribute(val name: String, val valueName: String)
