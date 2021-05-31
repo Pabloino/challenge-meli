3069a145760c00e4a35f724bad1a90bbc7bab1af
Pablo <pablo.inoriza@despegar.com>
2021-05-31 19:36:46 -0300

    Agrego los dos fragments

---
+++ app/src/main/java/com/meli/challengemeli/ui/ProductDetailsFragment.kt
@@ -0,0 +1,13 @@
+package com.meli.challengemeli.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.meli.challengemeli.R
+
+class ProductDetailsFragment : Fragment(R.layout.product_details_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/meli/challengemeli/ui/SearchFragment.kt
@@ -0,0 +1,13 @@
+package com.meli.challengemeli.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.meli.challengemeli.R
+
+class SearchFragment : Fragment(R.layout.search_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
