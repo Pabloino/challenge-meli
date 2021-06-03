95ee3bafb449c4cd369743c2d6eb8547c3690ad6
Pablo <pablo.inoriza@despegar.com>
2021-06-03 00:25:25 -0300

    Implemento la pantalla de detalles del producto

--- app/src/main/java/com/meli/challengemeli/ui/ProductDetailsFragment.kt
+++ app/src/main/java/com/meli/challengemeli/ui/ProductDetailsFragment.kt
@@ -3,11 +3,27 @@ package com.meli.challengemeli.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.meli.challengemeli.R
+import com.meli.challengemeli.databinding.ProductDetailsFragmentBinding
+import com.squareup.picasso.Picasso
 
 class ProductDetailsFragment : Fragment(R.layout.product_details_fragment) {
 
+    private lateinit var binding: ProductDetailsFragmentBinding
+    private val args by navArgs<ProductDetailsFragmentArgs>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = ProductDetailsFragmentBinding.bind(view)
+
+        with(args.product) {
+            binding.title.text = title
+            binding.price.text = "$${if ((price % 1) == 0.0) price.toInt() else price}"
+
+            Picasso.get()
+                .load(thumbnail)
+                .into(binding.imageProduct)
+        }
     }
 }
